135ff32c043544bdfdc3251775dc87353ed1be45
installer <installer@gmail.com>
2026-09-24 23:09:31 +0300

    try to pair jellufin and nextcloud 03

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -2,7 +2,7 @@
 
 let
   cloudMount02 = config.fbl.storage.cloud02.mountPoint;
-  mediaRoot = "${cloudMount02}";
+  mediaRoot = "${cloudMount02}/nextcloud-external/";
   port = config.fbl.ports.tcp.jellyfin;
   runtimePort = config.fbl.ports.tcp.jellyfinRuntime;
   lanInterface = config.fbl.network.interfaces.cr01Lan;
